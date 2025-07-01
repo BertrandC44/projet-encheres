57ce2bde4a810d81c348badde1564ed78777b64d
Camille-V98 <cam.v@hotmail.fr>
2025-07-01 14:25:30 +0200

    .

--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -1,11 +1,21 @@
 package fr.eni.encheres.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+
+import fr.eni.encheres.bo.Article;
 
 @Controller
 public class EncheresController {
+	
+	private EncheresService encheresService;
+	
+	public EncheresController(EncheresService encheresService) {
+		this.encheresService = encheresService;
+	}
 
 	@GetMapping("/")
 	public String index() {
@@ -27,7 +37,8 @@ public class EncheresController {
 
 
 	@GetMapping("/encheres/detail")
-	public String afficherDetailEnchere() {
+	public String afficherDetailEnchere(@RequestAttribute(name="id") long idArticle, Model model) {
+		Article article = encheresService.
 		return"enchere-en-cours";
 	}
 	
