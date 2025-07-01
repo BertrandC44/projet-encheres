f80e2cf02b3591546d8b3f6701344417ab5939b8
Laurine GROVEN <laurine.groven2025@campus-eni.fr>
2025-07-01 09:56:40 +0200

    ;

---
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -0,0 +1,39 @@
+package fr.eni.encheres.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EncheresController {
+
+	@GetMapping("/")
+	public String index() {
+		System.out.println("Clic vers Index");
+		return "encheres";
+	}
+	
+	@GetMapping("/connexion")
+	public String connexion() {
+		System.out.println("Clic vers Index");
+		return "connexion";
+	}
+	
+	@GetMapping("/inscription")
+	public String inscription() {
+		System.out.println("Clic vers Index");
+		return "inscription";
+	}
+
+	@GetMapping("/encheres")
+	public String encheres() {
+		System.out.println("afficher les enchères");
+		return "encheres";
+		
+	}
+	
+	@GetMapping("/vente")
+	public String vente() {
+		System.out.println("afficher les ventes");
+		return "vente";
+	}
+}
