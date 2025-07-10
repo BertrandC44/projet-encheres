025b0f8c75df32ed7d1c55b6557899c6ff1137df
BBR- CC <you@example.com>
2025-07-10 14:10:11 +0200

    m

--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.eni.encheres.bll.EncheresService;
 import fr.eni.encheres.bll.UtilisateurService;
