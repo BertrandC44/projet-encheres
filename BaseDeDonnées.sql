469dbd1a71ed50f07fc565d8f31c6192068d55a9
Camille-V98 <cam.v@hotmail.fr>
2025-07-10 11:05:05 +0200

    .

--- BaseDeDonnées.sql
+++ BaseDeDonnées.sql
@@ -1,17 +1,17 @@
 /*CREATE DATABASE [Encheres];*/
-
+ 
 DROP TABLE [ENCHERE];
 DROP TABLE [RETRAIT];
 DROP TABLE [ARTICLE];
+DROP TABLE [ROLES];
 DROP TABLE [UTILISATEUR];
 DROP TABLE [CATEGORIE];
-
-
-
-
+ 
+ 
+ 
 CREATE TABLE [UTILISATEUR](
-	[idUtilisateur] int  PRIMARY KEY IDENTITY,
-	[pseudo] [NVARCHAR](150) NOT NULL,
+	[idUtilisateur] int PRIMARY KEY IDENTITY,
+	[pseudo] [NVARCHAR](150) UNIQUE NOT NULL,
 	[nom] [NVARCHAR](150) NOT NULL,
 	[prenom] [NVARCHAR](150) NOT NULL,
 	[email] [NVARCHAR](250) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE [UTILISATEUR](
 	[credit] int NOT NULL,
 	[administrateur] bit NOT NULL,
 	);
-
+ 
 	
 CREATE TABLE [ARTICLE](
 	[idArticle] int PRIMARY KEY IDENTITY,
@@ -38,51 +38,67 @@ CREATE TABLE [ARTICLE](
 	[idUtilisateur] int NOT NULL,
 	[imageArticle] [VARCHAR](250),
 	)
-
-
+ 
+ 
 CREATE TABLE [ENCHERE](
+	[idEnchere] int PRIMARY KEY IDENTITY,
 	[dateEnchere] date NOT NULL,
 	[montantEnchere] int NOT NULL,
 	[idUtilisateur] int  NOT NULL,
 	[idArticle] int NOT NULL,
-	PRIMARY KEY ([idUtilisateur],[idArticle])
 	);
-
-
-
+ 
+ 
 CREATE TABLE [CATEGORIE](
 	[idCategorie] int PRIMARY KEY IDENTITY,
 	[libelle] [NVARCHAR](300) NOT NULL,
 	)
-
-
+ 
+ 
 CREATE TABLE [RETRAIT](
 	[idArticle] int ,
 	[rue] [NVARCHAR](200) NOT NULL,
 	[codePostal] VARCHAR (5) NOT NULL,
 	[ville] [NVARCHAR](150) NOT NULL,
-	
 	)
 
+CREATE TABLE [ROLES](
+	[pseudo] [NVARCHAR](150) NOT NULL,
+	[role] [VARCHAR](50) NOT NULL,
+	)
 
+ALTER TABLE ROLES add
+	CONSTRAINT PK_ROLES PRIMARY KEY (pseudo,role);
 
 ALTER TABLE ARTICLE add
 	CONSTRAINT FK_ARTICLE_UTILISATEUR FOREIGN KEY (idUtilisateur)
-	REFERENCES UTILISATEUR(idUtilisateur)ON DELETE CASCADE;
-
+	REFERENCES UTILISATEUR(idUtilisateur);
+ 
 ALTER TABLE ARTICLE add
 	CONSTRAINT FK_ARTICLE_CATEGORIE FOREIGN KEY (idCategorie)
-	REFERENCES CATEGORIE (idCategorie) ON DELETE CASCADE;
-
+	REFERENCES CATEGORIE (idCategorie);
+ 
 ALTER TABLE RETRAIT add
 	CONSTRAINT FK_RETRAIT_ARTICLE FOREIGN KEY (idArticle)
-	REFERENCES ARTICLE (idArticle) ON DELETE CASCADE;
-
+	REFERENCES ARTICLE (idArticle);
+ 
 ALTER TABLE ENCHERE add
 	CONSTRAINT FK_ENCHERE_UTILISATEUR FOREIGN KEY (idUtilisateur)
-	REFERENCES UTILISATEUR (idUtilisateur) ON DELETE CASCADE;
-
+	REFERENCES UTILISATEUR (idUtilisateur);
+ 
 ALTER TABLE ENCHERE add
 	CONSTRAINT FK_ENCHERE_ARTICLE FOREIGN KEY (idArticle)
-	REFERENCES ARTICLE (idArticle) ON DELETE CASCADE;
+	REFERENCES ARTICLE (idArticle);
 
+ALTER TABLE ROLES add
+	constraint FK_ROLES_UTILISATEURS foreign key (pseudo)
+	references UTILISATEUR(pseudo);
+
+
+ALTER TABLE CATEGORIE
+ADD image VARCHAR(255);
+
+UPDATE CATEGORIE SET image = 'clavier.jpg' WHERE idCategorie = 1;
+UPDATE CATEGORIE SET image = 'chaise-en-bois.jpg' WHERE idCategorie = 2;
+UPDATE CATEGORIE SET image = 'tondeuse.png' WHERE idCategorie = 3;
+UPDATE CATEGORIE SET image = 'tennis.png' WHERE idCategorie = 4;
--- Script.sql
+++ Script.sql
@@ -6,7 +6,7 @@ INSERT INTO UTILISATEUR (pseudo, nom, prenom, email, telephone, rue, codePostal,
 ('jtrillard', 'Trillard', 'Julien', 'jtrillard@campus-eni.fr', '0608091011', '22 avenue Victor', '33000', 'Bordeaux', 'Julien', 150, 0),
 ('sdautais', 'Dautais', 'Servane', 'sdautais@campus-eni.fr', '0611121314', '5 boulevard Haussmann', '31000', 'Toulouse', 'Servane', 50, 0);
 
--- Insertions catÃ©gories
+-- Insertions catégories
 
 INSERT INTO CATEGORIE (libelle) VALUES
 ('Informatique'),
@@ -17,9 +17,9 @@ INSERT INTO CATEGORIE (libelle) VALUES
 -- Insertions articles
 
 INSERT INTO ARTICLE (nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix, prixVente, etatVente, idCategorie, idUtilisateur, imageArticle) VALUES
-('Clavier', 'Clavier mÃ©canique AZERTY', '2025-07-01', '2025-07-10', 50, 0, 0, 1, 1, 'clavier.jpg'),
+('Clavier', 'Clavier mécanique AZERTY', '2025-07-01', '2025-07-10', 50, 0, 0, 1, 1, 'clavier.jpg'),
 ('Chaise', 'Chaise en bois massif', '2025-06-25', '2025-07-05', 30, 0, 0, 2, 2, 'chaise-en-bois.jpg'),
-('Tondeuse', 'Tondeuse Ã  gazon Ã©lectrique', '2025-07-03', '2025-07-12', 100, 0, 0, 3, 3, 'tondeuse.png'),
+('Tondeuse', 'Tondeuse à gazon électrique', '2025-07-03', '2025-07-12', 100, 0, 0, 3, 3, 'tondeuse.png'),
 ('Raquette', 'Raquette de tennis neuve', '2025-07-02', '2025-07-15', 70, 0, 0, 4, 4, 'tennis.png');
 
 -- Insertions retraits
@@ -30,11 +30,22 @@ INSERT INTO RETRAIT (idArticle, rue, codePostal, ville) VALUES
 (3, '22 avenue Victor', '33000', 'Bordeaux'),
 (4, '5 boulevard Haussmann', '31000', 'Toulouse');
 
--- Insertions enchÃ¨res
+-- Insertions enchères
 
 INSERT INTO ENCHERE (dateEnchere, montantEnchere, idUtilisateur, idArticle) VALUES
-('2025-07-02', 55, 2, 1),  -- Stephane enchÃ¨re sur Clavier
-('2025-07-03', 60, 3, 1),  -- Julien enchÃ¨re plus haute sur Clavier
+('2025-07-02', 55, 2, 1),  -- Stephane enchère sur Clavier
+('2025-07-03', 60, 3, 1),  -- Julien enchère plus haute sur Clavier
 ('2025-06-26', 35, 1, 2),  -- Annelise sur Chaise
 ('2025-07-04', 110, 4, 3), -- Servane sur Tondeuse
 ('2025-07-05', 75, 1, 4);  -- Annelise sur Raquette
+
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('abaille','ROLE_ACQUEREUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('abaille','ROLE_VENDEUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('sgobin','ROLE_ACQUEREUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('sgobin','ROLE_VENDEUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('sgobin','ROLE_ADMIN');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('jtrillard','ROLE_ACQUEREUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('jtrillard','ROLE_VENDEUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('sdautais','ROLE_ACQUEREUR');
+INSERT INTO [ROLES] ([pseudo],[role]) VALUES ('sdautais','ROLE_VENDEUR');
+
