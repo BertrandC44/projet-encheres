6d676a230ebc04543a9405019eafae18d165d576
Laurine GROVEN <laurine.groven2025@campus-eni.fr>
2025-07-11 10:17:47 +0200

    dernier commit du matin

--- BaseDeDonnées.sql
+++ BaseDeDonnées.sql
@@ -1,14 +1,11 @@
 /*CREATE DATABASE [Encheres];*/
- 
 DROP TABLE [ENCHERE];
 DROP TABLE [RETRAIT];
 DROP TABLE [ARTICLE];
 DROP TABLE [ROLES];
 DROP TABLE [UTILISATEUR];
 DROP TABLE [CATEGORIE];
- 
- 
- 
+
 CREATE TABLE [UTILISATEUR](
 	[idUtilisateur] int PRIMARY KEY IDENTITY,
 	[pseudo] [NVARCHAR](150) UNIQUE NOT NULL,
@@ -23,8 +20,7 @@ CREATE TABLE [UTILISATEUR](
 	[credit] int NOT NULL,
 	[administrateur] bit NOT NULL,
 	);
- 
-	
+
 CREATE TABLE [ARTICLE](
 	[idArticle] int PRIMARY KEY IDENTITY,
 	[nomArticle] [NVARCHAR](150) NOT NULL,
@@ -36,10 +32,9 @@ CREATE TABLE [ARTICLE](
 	[etatVente] int NOT NULL,
 	[idCategorie] int NOT NULL,
 	[idUtilisateur] int NOT NULL,
-	[imageArticle] [VARCHAR](250),
+	[imageArticle] [NVARCHAR](250),
 	)
- 
- 
+
 CREATE TABLE [ENCHERE](
 	[idEnchere] int PRIMARY KEY IDENTITY,
 	[dateEnchere] date NOT NULL,
@@ -47,58 +42,43 @@ CREATE TABLE [ENCHERE](
 	[idUtilisateur] int  NOT NULL,
 	[idArticle] int NOT NULL,
 	);
- 
- 
+
 CREATE TABLE [CATEGORIE](
 	[idCategorie] int PRIMARY KEY IDENTITY,
 	[libelle] [NVARCHAR](300) NOT NULL,
 	)
- 
- 
+
 CREATE TABLE [RETRAIT](
 	[idArticle] int ,
 	[rue] [NVARCHAR](200) NOT NULL,
 	[codePostal] VARCHAR (5) NOT NULL,
 	[ville] [NVARCHAR](150) NOT NULL,
 	)
-
+ 
 CREATE TABLE [ROLES](
 	[pseudo] [NVARCHAR](150) NOT NULL,
 	[role] [VARCHAR](50) NOT NULL,
 	)
-
+ 
 ALTER TABLE ROLES add
 	CONSTRAINT PK_ROLES PRIMARY KEY (pseudo,role);
-
+ 
 ALTER TABLE ARTICLE add
 	CONSTRAINT FK_ARTICLE_UTILISATEUR FOREIGN KEY (idUtilisateur)
 	REFERENCES UTILISATEUR(idUtilisateur);
- 
 ALTER TABLE ARTICLE add
 	CONSTRAINT FK_ARTICLE_CATEGORIE FOREIGN KEY (idCategorie)
 	REFERENCES CATEGORIE (idCategorie);
- 
 ALTER TABLE RETRAIT add
 	CONSTRAINT FK_RETRAIT_ARTICLE FOREIGN KEY (idArticle)
 	REFERENCES ARTICLE (idArticle);
- 
 ALTER TABLE ENCHERE add
 	CONSTRAINT FK_ENCHERE_UTILISATEUR FOREIGN KEY (idUtilisateur)
 	REFERENCES UTILISATEUR (idUtilisateur);
- 
 ALTER TABLE ENCHERE add
 	CONSTRAINT FK_ENCHERE_ARTICLE FOREIGN KEY (idArticle)
 	REFERENCES ARTICLE (idArticle);
-
+ 
 ALTER TABLE ROLES add
 	constraint FK_ROLES_UTILISATEURS foreign key (pseudo)
 	references UTILISATEUR(pseudo);
-
-
-ALTER TABLE CATEGORIE
-ADD image VARCHAR(255);
-
-UPDATE CATEGORIE SET image = 'clavier.jpg' WHERE idCategorie = 1;
-UPDATE CATEGORIE SET image = 'chaise-en-bois.jpg' WHERE idCategorie = 2;
-UPDATE CATEGORIE SET image = 'tondeuse.png' WHERE idCategorie = 3;
-UPDATE CATEGORIE SET image = 'tennis.png' WHERE idCategorie = 4;
--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -10,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.eni.encheres.bll.EncheresService;
@@ -142,9 +145,7 @@ public class EncheresController {
 		    } else {
 		    	enchereMin = montantMax + 1;
 		    }
-		    long idUtilisateurMontantMax = encheresService.idUtilisateurMontantMax(idArticle);
-		    String utilisateurMontantMax = encheresService.utilisateurMontantMax(idArticle);
-		    String categorieArticle = encheresService.categorieArticle(idArticle);
+		    
 		    String telephone = utilisateur.getTelephone();
 
 		    model.addAttribute("miseAPrix", miseAPrix);
@@ -155,7 +156,7 @@ public class EncheresController {
 		    model.addAttribute("montantMax", montantMax);
 		    String utilisateurMontantMax = encheresService.utilisateurMontantMax(idArticle);
 		    model.addAttribute("utilisateurMontantMax", utilisateurMontantMax);
-		    int enchereMin = montantMax + 1;   
+		    enchereMin = montantMax + 1;   
 		    model.addAttribute("enchereMin", enchereMin);
 		    long idUtilisateurMontantMax = encheresService.idUtilisateurMontantMax(idArticle);
 		    model.addAttribute("idUtilisateurMontantMax", idUtilisateurMontantMax); 
@@ -222,11 +223,6 @@ public class EncheresController {
 				}
 
 			}
-
-	}
-
-	
-
 
 	@PostMapping("/encheres/acquisition")
 	public String retraitArticle(@RequestParam(name = "idArticle") long idArticle, Model model) {	
